c1851928a4d35b38b9c956ac782881d5408f9c60
agaesh <agaeshk@gmail.com>
2024-11-23 14:47:37 +0800

    Initial Configuration of Laravel & React project is Ready

---
+++ public/js/app.js
@@ -0,0 +1,169 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\Users\\PC\\laravel\\resources\\js\\app.js: Support for the experimental syntax 'jsx' isn't currently enabled (22:9):\n\n\u001b[0m \u001b[90m 20 |\u001b[39m \u001b[36mfunction\u001b[39m \u001b[33mApp\u001b[39m() {\n \u001b[90m 21 |\u001b[39m     \u001b[36mreturn\u001b[39m (\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 22 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m    |\u001b[39m         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 23 |\u001b[39m             \u001b[33m<\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m\u001b[33mHello\u001b[39m \u001b[36mfrom\u001b[39m \u001b[33mReact\u001b[39m\u001b[33m!\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 24 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 25 |\u001b[39m     )\u001b[33m;\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\n\nIf you already added the plugin for this syntax to your config, it's possible that your config isn't being loaded.\nYou can re-run Babel with the BABEL_SHOW_CONFIG_FOR environment variable to show the loaded configuration:\n\tnpx cross-env BABEL_SHOW_CONFIG_FOR=C:\\Users\\PC\\laravel\\resources\\js\\app.js <your build command>\nSee https://babeljs.io/docs/configuration#print-effective-configs for more info.\n\n    at constructor (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:359:19)\n    at Parser.raise (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:3266:19)\n    at Parser.expectOnePlugin (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:3300:18)\n    at Parser.parseExprAtom (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10941:18)\n    at Parser.parseExprSubscripts (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10615:23)\n    at Parser.parseUpdate (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10600:21)\n    at Parser.parseMaybeUnary (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10580:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10434:61)\n    at Parser.parseExprOps (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10439:23)\n    at Parser.parseMaybeConditional (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10416:23)\n    at Parser.parseMaybeAssign (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10379:21)\n    at C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10349:39\n    at Parser.allowInAnd (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:11951:12)\n    at Parser.parseMaybeAssignAllowIn (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10349:17)\n    at Parser.parseParenAndDistinguishExpression (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:11210:28)\n    at Parser.parseExprAtom (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10863:23)\n    at Parser.parseExprSubscripts (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10615:23)\n    at Parser.parseUpdate (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10600:21)\n    at Parser.parseMaybeUnary (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10580:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10434:61)\n    at Parser.parseExprOps (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10439:23)\n    at Parser.parseMaybeConditional (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10416:23)\n    at Parser.parseMaybeAssign (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10379:21)\n    at Parser.parseExpressionBase (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10333:23)\n    at C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10329:39\n    at Parser.allowInAnd (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:11946:16)\n    at Parser.parseExpression (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:10329:17)\n    at Parser.parseReturnStatement (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12636:28)\n    at Parser.parseStatementContent (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12288:21)\n    at Parser.parseStatementLike (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12257:17)\n    at Parser.parseStatementListItem (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12237:17)\n    at Parser.parseBlockOrModuleBlockBody (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12810:61)\n    at Parser.parseBlockBody (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12803:10)\n    at Parser.parseBlock (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12791:10)\n    at Parser.parseFunctionBody (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:11634:24)\n    at Parser.parseFunctionBodyAndFinish (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:11620:10)\n    at C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12935:12\n    at Parser.withSmartMixTopicForbiddingContext (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:11928:14)\n    at Parser.parseFunction (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12934:10)\n    at Parser.parseFunctionStatement (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12619:17)\n    at Parser.parseStatementContent (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12281:21)\n    at Parser.parseStatementLike (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12257:17)\n    at Parser.parseModuleItem (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12234:17)\n    at Parser.parseBlockOrModuleBlockBody (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12810:36)\n    at Parser.parseBlockBody (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12803:10)\n    at Parser.parseProgram (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12131:10)\n    at Parser.parseTopLevel (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:12121:25)\n    at Parser.parse (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:13929:10)\n    at parse (C:\\Users\\PC\\laravel\\node_modules\\@babel\\parser\\lib\\index.js:13963:38)\n    at parser (C:\\Users\\PC\\laravel\\node_modules\\@babel\\core\\lib\\parser\\index.js:41:34)");
+
+/***/ }),
+
+/***/ "./resources/css/app.css":
+/*!*******************************!*\
+  !*** ./resources/css/app.css ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/app": 0,
+/******/ 			"css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
